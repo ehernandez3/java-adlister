180ad74f52fd64e0eac63badfef22d557b62cca1
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2018-12-11 15:13:38 -0600

    add index.jsp to ads directory, moved logic from login.jsp to LoginServlet.java

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,40 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+	@Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		if (req.getMethod().equalsIgnoreCase("post")) {
+			String username = req.getParameter("username");
+			String password = req.getParameter("password");
+			if (username.equals("admin") && password.equals("password")) {
+				res.sendRedirect("/profile");
+			}
+		}
+//				req.getRequestDispatcher("profile.jsp").forward(req, res);
+
+
+
+
+//    	String userName = req.getParameter("username");
+//    	String password = req.getParameter("password");
+//
+//    	if(userName.equalsIgnoreCase("admin") && password.equalsIgnoreCase("password")) {
+//			res.sendRedirect("/profile");
+//
+//		} else {
+//    		res.sendRedirect("/login");
+//		}
+
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		req.getRequestDispatcher("login.jsp").forward(req, res);
+    }
+}
