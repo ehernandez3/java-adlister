f16d00aa6bc19c6ad04f6e1b7c01193c1a5e10d5
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2018-12-12 09:39:17 -0600

    added refactored code to AdsShowServlet.java

--- src/main/java/AdsShowServlet.java
+++ src/main/java/AdsShowServlet.java
@@ -4,21 +4,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "AdsShowServlet", urlPatterns = "/ads")
 public class AdsShowServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		Ad a = new Ad(
-//			request.getParameter("title");
-//			request.getParameter();
-//		);
+
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		Ads adao = DaoFactory.getAdsDao();
-		request.setAttribute("ads", adao.all());
+		Ads AdsDao = DaoFactory.getAdsDao();
+		List<Ad> ads = AdsDao.all();
+		request.setAttribute("ads", ads);
+
+		//=========== OR
+
+//		Ads AdsDao = DaoFactory.getAdsDao();
+//		request.setAttribute("ads", AdsDao.all());
 
 
     	request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
