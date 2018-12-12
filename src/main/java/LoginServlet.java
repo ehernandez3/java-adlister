ead4dd81b7f9942c4d98df795e59b8925d49cf42
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2018-12-12 14:50:15 -0600

    add LogoutServlet.java, refactored LoginServlet.java and ViewProfileServlet.java

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,11 +3,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.swing.text.StyledEditorKit;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+
+        if (session.getAttribute("user") != null) {
+            response.sendRedirect("/profile");
+        }
+
+
         request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
@@ -15,11 +25,20 @@ public class LoginServlet extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
+		HttpSession session = request.getSession();
 
         if (validAttempt) {
+        	session.setAttribute("user", true);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
         }
     }
 }
+
+//	Lock down your profile page
+//
+//	-Ensure that users can only visit the /profile page if they are logged in.
+//	-In your LoginServlet, when a user successfully logs in, set a key named "user" in the session.
+//	-use this key to redirect unauthorized users that try to visit the /profile page.
+//	-if a user is already logged in and they visit /login, instead redirect them to their profile page
--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -6,15 +6,17 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
-public class ViewProfileServlet extends HttpServlet {
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
+		HttpSession session = request.getSession();
+		session.setAttribute("user", null);
 
-        if (session.getAttribute("user") == null) {
-            request.getRequestDispatcher("/login.jsp").forward(request, response);
-        }
+		request.getRequestDispatcher("/login.jsp").forward(request, response);
 
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+
     }
 }
